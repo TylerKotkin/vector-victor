bc2e5c42e6a87d89072cf58c34658146bf24f6ef
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-09 13:58:37 -0400

    Set-up and shape function

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,2 +1,27 @@
 class ShapeException(Exception):
     pass
+
+def shape(vector):
+    num_rows = len(vector)
+    return (num_rows,)
+
+def vector_add(vector):
+    pass
+
+def vector_sub(vector):
+    pass
+
+def vector_sum(vector):
+    pass
+
+def dot(vector):
+    pass
+
+def vector_multiply(vector):
+    pass
+
+def vector_mean(vector):
+    pass
+
+def shape(vector):
+    pass
