49de3a172708486b9e1f09a1f0ea6e3e15e3431a
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-09 18:52:28 -0400

    passing dot test

--- linear_algebra.py
+++ linear_algebra.py
@@ -25,17 +25,29 @@ def vector_sub(vector_1, vector_2):
     v_add = [vector_1[x] - vector_2[x] for x in range(len(vector_1))]
     return v_add
 
-# def vector_sum(vector):
-#     pass
-#
-# def dot(vector):
-#     pass
-#
+# def vector_sum(vector_1, vector_2, vector_3, vector_4,  vector_5):
+#      """vector_sum can take any number of vectors and add them together."""
+#      if len(vector_1) != len(vector_2):
+#          raise ShapeException
+
+
+def dot(vector_1, vector_2):
+    """
+    dot([a b], [c d])   = a * c + b * d
+
+    dot(Vector, Vector) = Scalar
+    """
+    if len(vector_1) != len(vector_2):
+        raise ShapeException
+
+    v_dot = sum([x * y for x, y in zip(vector_1, vector_2)])
+    return v_dot
+
 # def vector_multiply(vector):
 #     pass
 #
 # def vector_mean(vector):
 #     pass
 #
-# def shape(vector):
+# def magnitude(vector):
 #     pass
