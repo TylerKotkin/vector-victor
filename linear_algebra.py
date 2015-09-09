3a8206973d0eea52dd716f8b59a165bed192d494
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-09 15:30:27 -0400

    passing shape test

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,27 +1,28 @@
 class ShapeException(Exception):
     pass
 
-def shape(vector):
-    num_rows = len(vector)
-    return (num_rows,)
-
-def vector_add(vector):
-    pass
-
-def vector_sub(vector):
-    pass
-
-def vector_sum(vector):
-    pass
-
-def dot(vector):
-    pass
-
-def vector_multiply(vector):
-    pass
-
-def vector_mean(vector):
-    pass
 
 def shape(vector):
-    pass
+    v_shape = len(vector)
+    return (v_shape,)
+
+# def vector_add(vector):
+#     pass
+#
+# def vector_sub(vector):
+#     pass
+#
+# def vector_sum(vector):
+#     pass
+#
+# def dot(vector):
+#     pass
+#
+# def vector_multiply(vector):
+#     pass
+#
+# def vector_mean(vector):
+#     pass
+#
+# def shape(vector):
+#     pass
