874bb220c17aa585dd4089b1e4c29dada571c11b
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-09 21:14:36 -0400

    passing sum and mean tests. Not passing ShapeException test for the sum function.

--- linear_algebra.py
+++ linear_algebra.py
@@ -16,8 +16,9 @@ def vector_add(vector_1, vector_2):
     """
     if len(vector_1) != len(vector_2):
         raise ShapeException
-    v_add = [vector_1[x] + vector_2[x] for x in range(len(vector_1))]
-    return v_add
+
+    v_sum = [sum(x) for x in zip(vector_1, vector_2)]
+    return v_sum
 
 
 
@@ -27,10 +28,12 @@ def vector_sub(vector_1, vector_2):
     v_sub = [vector_1[x] - vector_2[x] for x in range(len(vector_1))]
     return v_sub
 
-# def vector_sum(vector_1, vector_2, vector_3, vector_4,  vector_5):
-#      """vector_sum can take any number of vectors and add them together."""
-#      if len(vector_1) != len(vector_2):
-#          raise ShapeException
+def vector_sum(*args):
+     """vector_sum can take any number of vectors and add them together."""
+
+
+     v_sum = [sum(x) for x in zip(*args)]
+     return v_sum
 
 
 def dot(vector_1, vector_2):
@@ -55,14 +58,16 @@ def vector_multiply(vector_1, y):
     v_mult = [x*y for x in vector_1]
     return v_mult
 
-# def vector_mean(vector):
-"""
-#     mean([a b], [c d]) = [mean(a, c) mean(b, d)]
-#
-#     mean(Vector)       = Vector
-#     """
-#     pass
-#
+def vector_mean(*args):
+     """
+     mean([a b], [c d]) = [mean(a, c) mean(b, d)]
+
+     mean(Vector)       = Vector
+     """
+     v_mean = [(sum(x)/len(args)) for x in zip(*args)]
+     return v_mean
+
+
 def magnitude(vector_1):
      """
      magnitude([a b])  = sqrt(a^2 + b^2)
